9ab7236dc83848a72878124053e42a791bd505ae
Карвацький Володимир <54777009+karvatskyvova@users.noreply.github.com>
2024-05-31 13:05:23 +0300

    added tests for recipe model

--- tests.py
+++ tests.py
@@ -9,3 +9,18 @@ class CategoryModelTest(TestCase):
         self.assertTrue(isinstance(self.category, Category))
         self.assertEqual(self.category.__str__(), 'Dessert')
 
+class RecipeModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Dessert')
+        self.recipe = Recipe.objects.create(
+            title='Chocolate Cake',
+            description='A delicious chocolate cake',
+            instructions='Mix ingredients and bake',
+            ingredients='Flour, sugar, cocoa, baking powder, eggs, milk, oil, vanilla extract',
+            category=self.category
+        )
+
+    def test_recipe_creation(self):
+        self.assertTrue(isinstance(self.recipe, Recipe))
+        self.assertEqual(self.recipe.__str__(), 'Chocolate Cake')
+        self.assertEqual(self.recipe.category.name, 'Dessert')
